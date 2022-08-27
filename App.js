f5eaaf9990b80cd4108b916992f7371b4d848118
Suparada Nontasri <6310110487@psu.ac.th>
2022-08-27 12:42:22 +0700

    Components and Image Background

--- App.js
+++ App.js
@@ -1,24 +1,22 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
+import Weather from './components/Weather';
+import Contansts from 'expo-constants';
+
 
 export default function App() {
-  const doIt = () => {
-    console.log("Hello from console")
-  }
+  
   return (
     <View style={styles.container}>
-      <Text onPress={doIt}>Hello world</Text>
+      <Weather zipCode="90110"/>
       <StatusBar style="auto" />
-    </View>
+ </View>
   );
  }
  
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    paddingTop: Contansts.statusBarHeight
   },
 });
---
+++ components/Weather.js
@@ -0,0 +1,26 @@
+import React, { useState } from 'react'
+import { ImageBackground, StyleSheet, Text } from 'react-native';
+import Forecast from './Forecast'
+
+export default function Weather(props) {
+    const [forecastInfo, setForecastInfo] = useState({
+    main: '-',
+    description: '-',
+    temp: 0
+    })
+    return (
+        <ImageBackground source={require('../1.png')} style={styles.backdrop}>
+            <Text>Zip Code</Text>
+            <Text>{props.zipCode}</Text>
+            <Forecast {...forecastInfo} />
+        </ImageBackground>
+    )
+   }
+
+   const styles = StyleSheet.create({
+    backdrop: {
+        alignItems: 'center',
+        width: '100%',
+        height: '100%'
+    }
+   })
